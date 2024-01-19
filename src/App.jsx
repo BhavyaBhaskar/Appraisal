e00f9d62af1ee3b782add8605f728494ed0a692b
BhavyaBhaskar <bhavyabhaskar04@gmail.com>
2024-01-20 01:18:05 +0530

    edit login

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import Acedemic from "./pages/Acedemic";
 import Research from "./pages/Research";
 import Administrative from "./pages/Administrative";
-import Login from "./pages/Login";
+
 import SignUp from "./pages/SignUp";
+import Login from "./pages/login";
 
 function App() {
   const [activeTab, setActiveTab] = useState("login");
